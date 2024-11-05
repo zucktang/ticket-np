90779ec839971da06505adffc1419581eecf6e3b
zucktang <zucktang@gmail.com>
2024-11-06 03:43:36 +0700

    fix bug signal

--- Ticket/apps/ticket/signals.py
+++ Ticket/apps/ticket/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from .models import Ticket, TicketLog
 
@@ -9,24 +9,27 @@ def log_ticket_changes(sender, instance, created, **kwargs):
             ticket=instance,
             action="Ticket created",
         )
-    else:
-        previous_ticket = Ticket.objects.get(pk=instance.pk)
-        if previous_ticket.status != instance.status:
-            TicketLog.objects.create(
-                ticket=instance,
-                action="Status changed",
-                previous_status=previous_ticket.status,
-                new_status=instance.status,
-            )
 
-        if previous_ticket.description != instance.description:
-            TicketLog.objects.create(
-                ticket=instance,
-                action="Description updated",
-            )
 
-        if previous_ticket.contact_info != instance.contact_info:
-            TicketLog.objects.create(
-                ticket=instance,
-                action="Contact information updated",
-            )
+@receiver(pre_save, sender=Ticket)
+def update_ticket_status(sender, instance, **kwargs):
+    previous_ticket = Ticket.objects.get(pk=instance.pk)
+    if previous_ticket.status != instance.status:
+        TicketLog.objects.create(
+            ticket=instance,
+            action="Status changed",
+            previous_status=previous_ticket.status,
+            new_status=instance.status,
+        )
+
+    if previous_ticket.description != instance.description:
+        TicketLog.objects.create(
+            ticket=instance,
+            action="Description updated",
+        )
+
+    if previous_ticket.contact_info != instance.contact_info:
+        TicketLog.objects.create(
+            ticket=instance,
+            action="Contact information updated",
+        )
