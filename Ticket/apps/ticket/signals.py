42fd7b1cc2de4c674931444db24b68cae4978f83
zucktang <zucktang@gmail.com>
2024-11-06 03:49:53 +0700

    refactor ticket log signal

--- Ticket/apps/ticket/signals.py
+++ Ticket/apps/ticket/signals.py
@@ -2,34 +2,26 @@ from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from .models import Ticket, TicketLog
 
-@receiver(post_save, sender=Ticket)
-def log_ticket_changes(sender, instance, created, **kwargs):
-    if created:
-        TicketLog.objects.create(
-            ticket=instance,
-            action="Ticket created",
-        )
+def log_change(ticket, action, **extra_fields):
+    TicketLog.objects.create(ticket=ticket, action=action, **extra_fields)
 
+@receiver(post_save, sender=Ticket)
+def log_ticket_creation(sender, instance, created, **kwargs):
+    if created:
+        log_change(instance, "Ticket created")
 
 @receiver(pre_save, sender=Ticket)
-def update_ticket_status(sender, instance, **kwargs):
-    previous_ticket = Ticket.objects.get(pk=instance.pk)
-    if previous_ticket.status != instance.status:
-        TicketLog.objects.create(
-            ticket=instance,
-            action="Status changed",
-            previous_status=previous_ticket.status,
-            new_status=instance.status,
-        )
+def log_ticket_updates(sender, instance, **kwargs):
+    if instance.pk:
+        previous_ticket = Ticket.objects.get(pk=instance.pk)
 
-    if previous_ticket.description != instance.description:
-        TicketLog.objects.create(
-            ticket=instance,
-            action="Description updated",
-        )
+        if previous_ticket.status != instance.status:
+            log_change(instance, "Status changed",
+                       previous_status=previous_ticket.status,
+                       new_status=instance.status)
 
-    if previous_ticket.contact_info != instance.contact_info:
-        TicketLog.objects.create(
-            ticket=instance,
-            action="Contact information updated",
-        )
+        if previous_ticket.description != instance.description:
+            log_change(instance, "Description updated")
+
+        if previous_ticket.contact_info != instance.contact_info:
+            log_change(instance, "Contact information updated")
